bf60693513032b3908e9a147bfe06b049d2cb58f
Tochi Mba <tochimba27@gmail.com>
2023-05-09 22:57:21 +0100

    all

---
+++ community/projectCard.php
@@ -0,0 +1,22 @@
+<li>
+    <div onclick="openDetails('<?php echo $i;?>')" class="card">
+        <img loop src="http://webtoolsv2/user_assets/image.php?v=private%2F500%20Best%20High%20Quality%20Tattoo%20Designs%2FTattoos%20(1).jpg" class="card__image" alt="" />
+        <div class="card__overlay">
+            <div class="card__header">
+                <svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
+                    <path />
+                </svg>
+                <img class="card__thumb" src="https://i.imgur.com/oYiTqum.jpg" alt="" />
+                <div class="card__header-text">
+                    <h3 class="card__title">Jessica Parker</h3>
+                    <span class="card__status">1 hour ago</span>
+                </div>
+            </div>
+            <p class="card__description">Pears are a type of fruit that
+                are enjoyed all over the world for their sweet and juicy flavor, as well as their many health
+                benefits. They come in a variety of colors, shapes, and sizes, and can be eaten raw, cooked, or
+                used in a variety of rsssssss
+            </p>
+        </div>
+    </div>
+</li>
